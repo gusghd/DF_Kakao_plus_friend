f2bd25f884916fbabc0c0e4fe8047be169e75346
BizFlow <iei>
2017-07-05 16:51:30 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -6,12 +6,36 @@ const router = express.Router();
 router.get('/keyboard', (req, res) => {
     const menu = {
         type: 'buttons',
-        buttons: ["메뉴1", "메뉴2", "메뉴3"]
+        buttons: ["카인", "시", "메뉴3"]
     };
 
     res.set({
         'content-type': 'application/json'
     }).send(JSON.stringify(menu));
+});
+
+router.post('/message', (req, res) => {
+    const _obj = {
+        user_key: req.body.user_key,
+        type: req.body.type,
+        content: req.body.content
+    };
+    let massage = {
+        "message": {
+            "text": '시간의문 20채널'
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": [
+                "메뉴1",
+                "메뉴2",
+                "메뉴3"
+            ]
+        }
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(massage));
 });
 
 router.get('/', (req, res) => {
