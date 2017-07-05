99e0bb5ea5d68729b1e9502cdc0d974fb34b15ef
BizFlow <iei>
2017-07-05 16:19:20 +0900

    ver1

---
+++ routes/bot.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/keyboard', (req, res) => {
+    const menu = {
+        type: 'buttons',
+        buttons: ["메뉴1", "메뉴2", "메뉴3"]
+    };
+
+res.set({
+    'content-type': 'application/json'
+}).send(JSON.stringify(menu));
+});
+
+router.get('/', (req, res) => {
+    console.log('aa')
+});
+
+module.exports = router;
+
+
