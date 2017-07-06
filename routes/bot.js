1259e0dc6529c6787db3f81d7cb13d142fcae9bb
BizFlow <iei>
2017-07-06 09:49:28 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const serverList = ["카인", "시로코", "카시야스", "안톤", "디레지에", "프레이", "힐더", "바칼"];
+let server;
 
 router.get('/keyboard', (req, res) => {
     const menu = {
@@ -17,24 +18,21 @@ router.get('/keyboard', (req, res) => {
 
 
 router.post('/message', (req, res) => {
-    console.log(req.body);
+    console.log(req.body.url);
     const _obj = {
         user_key: req.body.user_key,
         type: req.body.type,
         content: req.body.content
     };
-
+    if(!server) {
+        server = req.body.content;
+    }
     let massage = {
         "message": {
             "text": req.body.content + req.body.type
         },
         "keyboard": {
-            "type": "buttons",
-            "buttons": [
-                "메뉴1",
-                "메뉴2",
-                "메뉴3"
-            ]
+            "type": "text"
         }
     };
     res.set({
@@ -42,6 +40,15 @@ router.post('/message', (req, res) => {
     }).send(JSON.stringify(massage));
     res.redirect('/message');
 });
+
+/*router.post('/friend', checkUserKey, (req, res) => {
+    const user_key = req.body.user_key;
+    console.log(`${user_key}님이 쳇팅방에 참가했습니다.`);
+
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify({success: true}));
+});*/
 
 router.get('/', (req, res) => {
     console.log('aa')
