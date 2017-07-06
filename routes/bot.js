6b4703969f17a52756207c3d3c096f9530f8d3e4
BizFlow <iei>
2017-07-06 10:27:11 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -30,28 +30,41 @@ router.post('/message', (req, res) => {
     };
     let textContent = req.body.content;
     let keyboard;
-    if(!server) {
-        server = req.body.content;
-        textContent = "아이템을 입력해주세요";
-        keyboard =  {"type": "text"};
-    } else {
-        textContent = req.body.content + "의 추천채널은" + +" \n나오면 인증샷남겨주세요! ^^";
-        keyboard = {
-            "type": "buttons",
-            "buttons": serverList
+    let massage;
+    if(req.body.type == "text") {
+        if (!server) {
+            server = req.body.content;
+            textContent = "아이템을 입력해주세요";
+            keyboard = {"type": "text"};
+        } else {
+            textContent = req.body.content + "의 추천채널은" + +" \n나오면 인증샷남겨주세요! ^^";
+            keyboard = {
+                "type": "buttons",
+                "buttons": serverList
+            };
+            server = undefined;
+        }
+        massage = {
+            "message": {
+                "text": textContent
+            },
+            "keyboard": keyboard
         };
-        server = undefined;
+
+    } else {
+        massage = {
+            "massage": "잘못된 타입의 데이터입니다.",
+            "keyboard" : {
+                "type": "buttons",
+                "buttons": serverList
+            }
+        }
     }
-    let massage = {
-        "message": {
-            "text": textContent
-        },
-        "keyboard": keyboard
-    };
     res.set({
         'content-type': 'application/json'
     }).send(JSON.stringify(massage));
     res.redirect('/message');
+
 });
 
 /*router.post('/friend', checkUserKey, (req, res) => {
