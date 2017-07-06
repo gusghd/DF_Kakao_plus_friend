8e81518ffe8a45545f508916b224f0aa27abb835
BizFlow <iei>
2017-07-06 15:02:38 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -30,7 +30,7 @@ router.post('/message', (req, res) => {
         type: req.body.type,
         content: req.body.content
     };
-    let massage = {
+    let message = {
         "message":{},
         "keyboard": {}
     };
@@ -40,34 +40,30 @@ router.post('/message', (req, res) => {
     if(req.body.type == "text") {
         if (!server) {
             server = req.body.content;
-            massage.message.text = "아이템을 입력해주세요";
-            massage.keyboard = {"type": "text"};
+            message.message.text = "아이템을 입력해주세요";
+            message.keyboard = {"type": "text"};
         } else {
             selectedChannel = bot.choseChannel(server);
-            massage.massage = {
+            message.message = {
                 "text" : req.body.content + "의 추천채널은 " + selectedChannel +"입니다. \n뜬다면 메가폰 한번 날려줘요! ^^",
                 "photo" : {
                     "url": "http://hell.cafe24app.com/images/beam.jpg",
                     "width": 640,
                     "height": 480
-                },
-                "message_button": {
-                    "label": "라벨입니다",
-                    "url": "https://cheese10yun.github.io/"
                 }
             };
-            massage.keyboard = {
+            message.keyboard = {
                 "type": "buttons",
                 "buttons": serverList
             };
             server = undefined;
         }
     } else {
-        massage.massage.text = "잘못된 타입의 데이터입니다."
+        message.message.text = "잘못된 타입의 데이터입니다."
     }
     res.set({
         'content-type': 'application/json'
-    }).send(JSON.stringify(massage));
+    }).send(JSON.stringify(message));
     res.redirect('/message');
 
 });
