203faff3d849cf804a71e05afa8f5b12879d5df9
BizFlow <iei>
2017-07-06 14:51:08 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -47,7 +47,7 @@ router.post('/message', (req, res) => {
             massage.massage = {
                 "text" : req.body.content + "의 추천채널은 " + selectedChannel +"입니다. \n뜬다면 메가폰 한번 날려줘요! ^^",
                 "photo" : {
-                    "url": "http://hell.cafe24app.com/images/beam.png",
+                    "url": "http://hell.cafe24app.com/images/beam.jpg",
                     "width": 640,
                     "height": 480
                 }
