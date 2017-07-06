e97cbfab6a7b74c0c319ec23a6f2ff67e31a082c
BizFlow <iei>
2017-07-06 14:22:42 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -36,17 +36,16 @@ router.post('/message', (req, res) => {
     if(req.body.type == "text") {
         if (!server) {
             server = req.body.content;
-            massage.message = "아이템을 입력해주세요";
+            massage.message.text = "아이템을 입력해주세요";
             massage.keyboard = {"type": "text"};
-
         } else {
             selectedChannel = bot.choseChannel(server);
-            massage.massage = req.body.content + "의 추천채널은 " + selectedChannel +"입니다. \n뜬다면 메가폰 한번 날려줘요! ^^";
+            massage.massage.text = req.body.content + "의 추천채널은 " + selectedChannel +"입니다. \n뜬다면 메가폰 한번 날려줘요! ^^";
             massage.keyboard = {
                 "type": "buttons",
                 "buttons": serverList
             };
-            massage.photo = {
+            massage.massage.photo = {
                 "url": "http://hell.cafe24app.com/images/beam.png",
                 "width": 640,
                 "height": 480
@@ -54,7 +53,7 @@ router.post('/message', (req, res) => {
             server = undefined;
         }
     } else {
-        massage = "잘못된 타입의 데이터입니다."
+        massage.massage = "잘못된 타입의 데이터입니다."
     }
     res.set({
         'content-type': 'application/json'
