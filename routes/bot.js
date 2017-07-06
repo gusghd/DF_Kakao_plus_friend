c1eea70ab9b5efa90bbe91b801ff104c64c79aa1
BizFlow <iei>
2017-07-06 14:36:12 +0900

    ver1

--- routes/bot.js
+++ routes/bot.js
@@ -46,7 +46,7 @@ router.post('/message', (req, res) => {
             massage.massage = {
                 "text" : req.body.content + "의 추천채널은 " + selectedChannel +"입니다. \n뜬다면 메가폰 한번 날려줘요! ^^",
                 "photo" : {
-                    "url": "/images/beam.png",
+                    "url": "images/beam.png",
                     "width": 640,
                     "height": 480
                 }
@@ -58,7 +58,7 @@ router.post('/message', (req, res) => {
             server = undefined;
         }
     } else {
-        massage.massage = "잘못된 타입의 데이터입니다."
+        massage.massage.text = "잘못된 타입의 데이터입니다."
     }
     res.set({
         'content-type': 'application/json'
