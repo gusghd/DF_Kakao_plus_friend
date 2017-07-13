ae1763a6b6aa507661066d07ecc321eab74b54d4
BizFlow <iei>
2017-07-13 10:10:59 +0900

    ver1

--- web.js
+++ web.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 var allowCORS = function(req, res, next) {
-  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   (req.method === 'OPTIONS') ?
