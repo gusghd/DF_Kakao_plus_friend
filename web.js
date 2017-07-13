d88a68f3e890a903435493f1c2e2cf6599ba9dd3
BizFlow <iei>
2017-07-13 10:08:52 +0900

    ver1

--- web.js
+++ web.js
@@ -21,6 +21,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+var allowCORS = function(req, res, next) {
+  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  (req.method === 'OPTIONS') ?
+      res.send(200) :
+      next();
+};
+
+// 이 부분은 app.use(router) 전에 추가하도록 하자
+app.use(allowCORS);
+
+
 app.use('/', bot);
 
 // catch 404 and forward to error handler
