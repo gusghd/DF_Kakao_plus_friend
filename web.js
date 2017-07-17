31f0923d6b5b03bdf3dfc23383811a8448e39b2b
BizFlow <iei>
2017-07-17 14:57:13 +0900

    ver1

--- web.js
+++ web.js
@@ -12,8 +12,10 @@ var bot = require('./routes/bot');
 var app = express();
 
 app.use(cors);
-app.use((req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // uncomment after placing your favicon in /public
